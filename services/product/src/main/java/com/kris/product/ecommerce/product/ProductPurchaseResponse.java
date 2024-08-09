fcd8780490721904e63d7e5bb076d83fcdfc24ac
Kristiyan Kiryakov <126320553+kristiyankiryakov@users.noreply.github.com>
2024-08-09 20:20:44 +0300

    implement product-service

---
+++ services/product/src/main/java/com/kris/product/ecommerce/product/ProductPurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.kris.product.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public record ProductPurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
---
+++ services/product/src/main/java/com/kris/product/ecommerce/product/ProductResponse.java
@@ -0,0 +1,15 @@
+package com.kris.product.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(
+        Integer id,
+        String name,
+        String description,
+        double availableQuantity,
+        BigDecimal price,
+        Integer categoryId,
+        String CategoryName,
+        String CategoryDescription
+) {
+}
